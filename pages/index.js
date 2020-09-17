eafb0f2dfba3eb00aac037cc4e51ccb83b1794d3
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-17 14:51:24 -0700

    new layout

--- pages/index.js
+++ pages/index.js
@@ -19,33 +19,26 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <form>
+            {/* Height entry */}
+            <label>
+              <h1>Enter your height:</h1>
+            </label>
+            <input className={styles.height} type="text" name="height" placeholder="1.80 cm" size="50"></input>
+  
+            {/* Weight entry */}
+            <label>
+              <h1>
+                Enter your weight:
+              </h1></label>
+            <input className={styles.height} type="text" name="weight" placeholder=" 100 kg" size="50"></input><br /><br />
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            {/* Calculate button */}
+            <button className={styles.calcBtn}>Calculate BMI</button>
+          </form>
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+
         </div>
       </main>
 
