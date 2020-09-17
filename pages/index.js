4c31404a4bfe8b7401deed7f59c79988955e8b26
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-17 12:13:34 -0700

    app setup

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,17 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>BMI Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          BMI Calculator
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Calculate your BMI
         </p>
 
         <div className={styles.grid}>
