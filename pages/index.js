74fa7333cb6d91a7acbeeb6b8cc633b21604d14b
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-19 12:49:36 -0700

    material-ui

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {useState} from 'react'
+import { useState } from 'react'
+import CopyrightIcon from '@material-ui/icons/Copyright';
 
 export default function Home() {
   const [info, setInfo] = useState({
@@ -78,14 +79,7 @@ export default function Home() {
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <CopyrightIcon />Copyright 2020
       </footer>
     </div>
   )
